a4a32662a9e749d329f16979e00fc470c60bd449
thalesAlves758 <thalesalves758@gmail.com>
2023-01-17 18:53:51 -0300

    adicionar rota de pegar disciplinas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Login from './components/pages/Login';
 import AlterarSenha from './components/pages/AlterarSenha';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicial from './components/pages/Inicial';
+import Cabecalho from './components/shared/Cabecalho';
+import Disciplinas from './components/pages/Disciplinas';
 
 function App() {
   return (
     <BrowserRouter>
+      <Cabecalho />
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/alterarSenha' element={<AlterarSenha />} />
+        <Route path='/' element={<Inicial />} />
+        <Route path='/disciplinas' element={<Disciplinas />} />
       </Routes>
     </BrowserRouter>
   );
