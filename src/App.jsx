aacbacf77a237e4670185935a84ab43106ae4b12
thalesAlves758 <thalesalves758@gmail.com>
2023-01-18 21:11:06 -0300

    adicionar UsuarioContext.Provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicial from './components/pages/Inicial';
 import Cabecalho from './components/shared/Cabecalho';
 import Disciplinas from './components/pages/Disciplinas';
+import { useEffect, useState } from 'react';
+import UsuarioContext from './contexts/UsuarioContext';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+  const [storedUsuario, setStoredUsuario] = useLocalStorage("usuario", null);
+  const [usuario, setUsuario] = useState(storedUsuario);
+
+  useEffect(() => {
+    setStoredUsuario(usuario);
+  }, [usuario]);
+
   return (
     <BrowserRouter>
-      <Cabecalho />
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/alterarSenha' element={<AlterarSenha />} />
-        <Route path='/' element={<Inicial />} />
-        <Route path='/disciplinas' element={<Disciplinas />} />
-      </Routes>
+      <UsuarioContext.Provider value={{ usuario, setUsuario }}>
+        <Cabecalho />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/alterarSenha' element={<AlterarSenha />} />
+          <Route path='/' element={<Inicial />} />
+          <Route path='/disciplinas' element={<Disciplinas />} />
+        </Routes>
+      </UsuarioContext.Provider>
     </BrowserRouter>
   );
 }
