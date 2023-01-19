59a43c8def1c9a00f60e1890cb0db8f944606310
thalesAlves758 <thalesalves758@gmail.com>
2023-01-18 21:09:40 -0300

    alterar nome do metodo

--- src/hooks/api/usePegarDisciplinas.js
+++ src/hooks/api/usePegarDisciplinas.js
@@ -2,7 +2,7 @@ import useAsync from '../useAsync';
 
 import * as disciplinaApi from '../../services/disciplinaApi';
 
-export default function useLogin() {
+export default function usePegarDisciplinas() {
   const {
     status,
     result,
