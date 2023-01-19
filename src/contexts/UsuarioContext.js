f99fe915e9a80155b184e28c79d569d2c5e4abd7
thalesAlves758 <thalesalves758@gmail.com>
2023-01-18 21:10:28 -0300

    alterar nome da variavel

--- src/contexts/UsuarioContext.js
+++ src/contexts/UsuarioContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const UserContext = createContext();
+const UsuarioContext = createContext();
 
-export default UserContext;
+export default UsuarioContext;
