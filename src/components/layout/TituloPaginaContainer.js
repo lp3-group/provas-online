47073f742f218f0bcc75ec94a09a7e916b10a72e
thalesAlves758 <thalesalves758@gmail.com>
2023-01-25 22:02:33 -0300

    criar componente de layout TituloPaginaContainer

---
+++ src/components/layout/TituloPaginaContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TituloPaginaContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 10px 0;
+`;
+
+export default TituloPaginaContainer;
