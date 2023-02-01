f1a378aa431bb93a32fd19c4aad6bb8da1ff1f82
thalesAlves758 <thalesalves758@gmail.com>
2023-02-01 09:05:28 -0300

    adicionar modal de formulario de criacao de estudante

--- src/components/pages/Estudantes/index.jsx
+++ src/components/pages/Estudantes/index.jsx
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import UsuarioContext from "../../../contexts/UsuarioContext";
 import usePegarUsuarios from '../../../hooks/api/usePegarUsuarios';
+import httpStatus from "../../../utils/httpStatus";
+import ModalFormEstudante from "./ModalFormEstudante";
 
 function Estudantes() {
   const navigate = useNavigate();
@@ -11,6 +13,8 @@ function Estudantes() {
   const { error, pegarUsuarios, result, status } = usePegarUsuarios();
 
   const { usuario } = useContext(UsuarioContext);
+
+  const [showEstudanteFormModal, setShowEstudanteFormModal] = useState(false);
 
   useEffect(() => {
     pegarUsuarios({ token: usuario.token });
@@ -23,53 +27,57 @@ function Estudantes() {
   }, [status]);
 
   return (
-    <Container className="my-3 col-sm-10">
-      <TituloPaginaContainer>
-          <h1>Estudantes</h1>
+    <>
+      <Container className="my-3 col-sm-10">
+        <TituloPaginaContainer>
+            <h1>Estudantes</h1>
 
-          <div>
-            <Button>
-              Cadastrar Estudante
-            </Button>
-          </div>
-        </TituloPaginaContainer>
+            <div>
+              <Button onClick={() => setShowEstudanteFormModal(true)}>
+                Cadastrar Estudante
+              </Button>
+            </div>
+          </TituloPaginaContainer>
 
 
-        { result && result.data && result.data.estudantes.length > 0 ? <Table size="sm" striped bordered hover>
-          <thead>
-            <tr>
-              <th className='col-sm-1'>#</th>
-              <th className='col-sm-3'>Nome</th>
-              <th className='col-sm-3'>Sobrenome</th>
-              <th className='col-sm-2'>Nome de Usuário</th>
-              <th className='col-sm-2 text-center'>Matrícula</th>
-              <th className='col-sm-1 text-center'>Opções</th>
-            </tr>
-          </thead>
-          <tbody>
-            {result.data.estudantes.map((estudante, indice) => {
-              return (
-                <tr key={estudante.id}>
-                  <td className='col-sm-1'>{indice + 1}</td>
-                  <td className='col-sm-3'>{estudante.nome}</td>
-                  <td className='col-sm-3 text-center'>{estudante.sobrenome}</td>
-                  <td className='col-sm-2 text-center'>{estudante.nomeUsuario}</td>
-                  <td className='col-sm-2 text-center'>{estudante.matricula}</td>
-                  <td className='col-sm-1 text-center'>
-                    <Button variant="danger">Excluir</Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-          <tfoot>
-            <tr>
-              <th colSpan={2}>Total</th>
-              <td className='text-center'>{result.data.estudantes.length} resultado(s)</td>
-            </tr>
-          </tfoot>
-        </Table> : <h6>Nenhum estudante encontrado</h6> }
-    </Container>
+          { result && result.data && result.data.estudantes.length > 0 ? <Table size="sm" striped bordered hover>
+            <thead>
+              <tr>
+                <th className='col-sm-1'>#</th>
+                <th className='col-sm-3'>Nome</th>
+                <th className='col-sm-2'>Sobrenome</th>
+                <th className='col-sm-2'>Nome de Usuário</th>
+                <th className='col-sm-2'>Matrícula</th>
+                <th className='col-sm-2 text-center'>Opções</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.data.estudantes.map((estudante, indice) => {
+                return (
+                  <tr key={estudante.id}>
+                    <td className='col-sm-1'>{indice + 1}</td>
+                    <td className='col-sm-3'>{estudante.nome}</td>
+                    <td className='col-sm-2'>{estudante.sobrenome}</td>
+                    <td className='col-sm-2'>{estudante.nomeUsuario}</td>
+                    <td className='col-sm-2'>{estudante.matricula}</td>
+                    <td className='col-sm-2 text-center'>
+                      <Button variant="danger">Excluir</Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+            <tfoot>
+              <tr>
+                <th colSpan={5}>Total</th>
+                <td className='text-center'>{result.data.estudantes.length} resultado(s)</td>
+              </tr>
+            </tfoot>
+          </Table> : <h6>Nenhum estudante encontrado</h6> }
+      </Container>
+
+      <ModalFormEstudante pegarUsuarios={pegarUsuarios} show={showEstudanteFormModal} onHide={() => setShowEstudanteFormModal(false)} modalTitle="Cadastrar estudante" />
+    </>
   )
 }
 
