006882fcaa2a08be6364e4c31d465cfddd5efd79
thalesAlves758 <thalesalves758@gmail.com>
2023-01-31 22:20:03 -0300

    adicionar protecao de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Login from './components/pages/Login';
 import AlterarSenha from './components/pages/AlterarSenha';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Inicial from './components/pages/Inicial';
 import Cabecalho from './components/shared/Cabecalho';
 import Disciplinas from './components/pages/Disciplinas';
@@ -9,6 +9,14 @@ import { useEffect, useState } from 'react';
 import UsuarioContext from './contexts/UsuarioContext';
 import useLocalStorage from './hooks/useLocalStorage';
 import FormProva from './components/pages/FormProva';
+
+function ProtectedRoutes({ user, redirectPath = '/login' }) {
+  if(!user) {
+    return <Navigate to={redirectPath} />;
+  }
+
+  return <Outlet />;
+}
 
 function App() {
   const [storedUsuario, setStoredUsuario] = useLocalStorage("usuario", null);
@@ -24,12 +32,15 @@ function App() {
         <Cabecalho />
         <Routes>
           <Route path='/login' element={<Login />} />
-          <Route path='/alterarSenha' element={<AlterarSenha />} />
-          <Route path='/' element={<Inicial />} />
-          <Route path='/disciplinas' element={<Disciplinas />} />
-          <Route path='/provas' element={<Provas />} />
-          <Route path='/provas/formulario' element={<FormProva />} />
-          <Route path='/provas/formulario/:provaId/editar' element={<FormProva />} />
+
+          <Route element={<ProtectedRoutes user={usuario} />}>
+            <Route path='/alterarSenha' element={<AlterarSenha />} />
+            <Route path='/' element={<Inicial />} />
+            <Route path='/disciplinas' element={<Disciplinas />} />
+            <Route path='/provas' element={<Provas />} />
+            <Route path='/provas/formulario' element={<FormProva />} />
+            <Route path='/provas/formulario/:provaId/editar' element={<FormProva />} />
+          </Route>
         </Routes>
       </UsuarioContext.Provider>
     </BrowserRouter>
