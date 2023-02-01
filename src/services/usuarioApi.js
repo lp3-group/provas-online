e9930485fef1ef8fd598356c7f3e320af1a2a254
thalesAlves758 <thalesalves758@gmail.com>
2023-02-01 09:04:41 -0300

    adicionar metodo para criar usuario

--- src/services/usuarioApi.js
+++ src/services/usuarioApi.js
@@ -9,3 +9,13 @@ export async function pegarUsuarios({ token }) {
 
   return { data, status };
 }
+
+export async function criarUsuario({ token, matricula, nome, sobrenome, senha, nomeUsuario, tipoUsuario }) {
+  const { data, status } = await api.post('/usuarios', { matricula, nome, sobrenome, senha, nomeUsuario, tipoUsuario }, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return { data, status };
+}
